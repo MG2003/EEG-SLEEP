914eea79b152dabfffb0aa4de1b984408506a5e3
MG2003 <33848256+MG2003@users.noreply.github.com>
2022-04-27 00:04:59 -0400

    Finished bluetooth connection and added status box to GUI

--- bandpower.py
+++ bandpower.py
@@ -11,33 +11,50 @@ def run():
     
   
     BoardShim.enable_dev_board_logger()
-
+    """
     parser = argparse.ArgumentParser()
     # use docs to check which parameters are required for specific board, e.g. for Cyton - set serial port
+    
     parser.add_argument('--timeout', type=int, help='timeout for device discovery or connection', required=False,
-                        default=15)
-
-    parser.add_argument('--serial-port', type=str, help='serial port', required=False, default='')
-    parser.add_argument('--mac-address', type=str, help='mac address', required=False, default='')
- 
-    parser.add_argument('--board-id', type=int, required = False, help='board id, check docs to get a list of supported boards')
-
+                        default=0)
+    parser.add_argument('--ip-port', type=int, help='ip port', required=False, default=0)
+    parser.add_argument('--ip-protocol', type=int, help='ip protocol, check IpProtocolType enum', required=False,
+                        default=0)
+    parser.add_argument('--ip-address', type=str, help='ip address', required=False, default='')
+    parser.add_argument('--serial-port', type=str, help='serial port', required=True, default='')
+    parser.add_argument('--mac-address', type=str, help='mac address', required=True, default='')
+    parser.add_argument('--other-info', type=str, help='other info', required=False, default='')
+    parser.add_argument('--streamer-params', type=str, help='streamer params', required=False, default='')
+    parser.add_argument('--serial-number', type=str, help='serial number', required=False, default='')
+    parser.add_argument('--board-id', type=int, help='board id, check docs to get a list of supported boards',
+                        required=True)
+    parser.add_argument('--file', type=str, help='file', required=False, default='')
     args = parser.parse_args()
+    """
+
     params = BrainFlowInputParams()
-    params.serial_port = 'dongle serial port(COM3, /dev/ttyUSB0…)'
+    params.ip_port = 0
+    params.serial_port = 'COM3'
     params.mac_address = 'c7:86:fd:64:9c:e0'
-    params.timeout = '15'
+    params.other_info = ""
+    params.serial_number = ""
+    params.ip_address = ""
+    params.ip_protocol = 0
+    params.timeout = 0
+
 
     board = BoardShim(1, params)
+    board_descr = BoardShim.get_board_descr(1)
+    #print(board_descr)
     master_board_id = 1
-    board_descr = BoardShim.get_board_descr(args.board_id)
     sampling_rate = BoardShim.get_sampling_rate(master_board_id)
     board.prepare_session()
+
     # use synthetic board for demo
     """
     params = BrainFlowInputParams()
     board_id = BoardIds.SYNTHETIC_BOARD.value
-    board_descr = BoardShim.get_board_descr(board_id)
+    
     sampling_rate = int(board_descr['sampling_rate'])
     board = BoardShim(board_id, params)
     board.prepare_session()
@@ -57,7 +74,7 @@ def run():
 
     eeg_channels = board_descr['eeg_channels']
     # second eeg channel of synthetic board is a sine wave at 10Hz, should see huge alpha
-    eeg_channel = eeg_channels[0]
+    eeg_channel = eeg_channels[1]
     # optional detrend
     DataFilter.detrend(data[eeg_channel], DetrendOperations.LINEAR.value)
     psd = DataFilter.get_psd_welch(data[eeg_channel], nfft, nfft // 2, sampling_rate,
@@ -66,13 +83,12 @@ def run():
     band_power_alpha = DataFilter.get_band_power(psd, 7.0, 13.0)
     band_power_theta = DataFilter.get_band_power(psd, 4.0, 7.0)
     print("theta/alpha:%f", band_power_theta / band_power_alpha)
-  
+    sendtogui = {
+    "theta": band_power_theta,
+    "alpha": band_power_alpha,
 
-    # fail test if ratio is not smth we expect
+    }
 
-    if (band_power_theta / band_power_alpha < 100):
-        return 0
-    else:
-        return 1
+    return sendtogui
     
 
--- process.py
+++ process.py
@@ -3,8 +3,9 @@ import tkinter
 from tkinter import *
 from tkinter import Button, GROOVE
 from threading import *
+import time
 
-
+starttime = time.time()
 
 
 clicked = False
@@ -12,22 +13,22 @@ clicked = False
 def click():
     global clicked
     clicked = True
-
+def get_timestamp():
+    return str(round(time.time()-starttime, 2))
 def threading():
     # Call work function
+    information.insert("end", get_timestamp() + ": Starting Data Collection\n")
     t1=Thread(target=proc)
     t1.start()
 def proc():
     global clicked
     start['state'] = 'disabled'
-
-    if(bandpower.run()==0):
-        print('success')
-
-    else:
-        print('fail')
-        
-
+    info = bandpower.run()     
+    information.insert("end", get_timestamp() + ": Data Received\n")
+    information.insert("end", "Alpha band power: " + str(round(info["alpha"],2))+ "\n")
+    information.insert("end", "Theta band power: " + str(round(info["theta"],2))+"\n")
+    information.insert("end", "Alpha/Theta Ratio: " + str(round(info["alpha"]/info["theta"], 2)) + "\n")
+    information.see("end")
     if(clicked == False):    
         proc()
     else:
@@ -38,16 +39,25 @@ def proc():
         
    
         
+fontfamily = {
+    "main": 8,
+    "title": 17
 
+}
 
 root = Tk()
-root.geometry("400x400") 
-label = Label(root, text = "EEG Sleep Diagnosis", font = ("Arial", 17))
-start = Button(root, text="START COLLECTION", command = threading, relief = GROOVE, width = 17, height = 3)
-end = Button(root, text = "STOP COLLECTION", command = click, relief = GROOVE, width = 17, height = 3)
+root.geometry("350x350") 
+label = Label(root, text = "EEG Sleep Diagnosis", font = ("Arial", fontfamily["title"]))
+instructions = Label(root, text="Insert bluetooth dongle into computer, \nmake sure EEG is on, and press start collection")
+information = Text(root, borderwidth= 2, relief = GROOVE, width = 40, height = 10)
+start = Button(root, text="START COLLECTION", command = threading, relief = GROOVE, width = 17, height = 3, font = ("Arial", fontfamily["main"]))
+end = Button(root, text = "STOP COLLECTION", command = click, relief = GROOVE, width = 17, height = 3, font = ("Arial", fontfamily["main"]))
 label.pack(padx = 10, pady = 10)
-start.pack(padx = 30, pady = 10, side = tkinter.LEFT)
-end.pack(padx = 30, pady = 10, side = tkinter.LEFT)
+instructions.pack()
+information.pack(pady = 5)
+start.pack(padx = 30, side = LEFT)
+end.pack(padx = 30, side = RIGHT)
+
 
 
 root.mainloop()
